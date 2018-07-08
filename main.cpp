a529bcb828a6f65be8e30459b8baa17263224fe4
Petr Stenin <petr.stenin@gmail.com>
2018-07-08 20:26:37 +1000

    Initial commit - lets start app about MLM!

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "mainwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication setevikApp(argc, argv);
+    MainWindow mainWin;
+    mainWin.show();
+
+    return setevikApp.exec();
+}
